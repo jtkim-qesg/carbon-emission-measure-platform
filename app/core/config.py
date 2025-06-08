bff312b25ad3f80c120c921a9c8dc288c6387d26
jtkim-qesg <jtkim@qesg.co.kr>
2025-06-09 02:17:33 +0900

    Create feature of JWT System

--- app/core/config.py
+++ app/core/config.py
@@ -25,7 +25,10 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
     SYNC_DATABASE_URL: str
-    # SECRET_KEY: str
+    
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     # REDIS_URL: str
 
     class Config:
