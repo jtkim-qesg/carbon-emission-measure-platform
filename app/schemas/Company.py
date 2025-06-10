b1bb53f7e75dd6eef29cff0852744b0972d0c430
jtkim-qesg <jtkim@qesg.co.kr>
2025-06-10 18:37:27 +0900

    Create a feature of Company's shemas and Admin Company's endpoints

--- app/schemas/Company.py
+++ app/schemas/Company.py
@@ -1,1 +1,25 @@
-# from pydantic import BaseModel
+# from pydantic import BaseModelfrom pydantic import BaseModel, ConfigDict, EmailStr
+from enum import Enum
+from pydantic import BaseModel, ConfigDict
+from app.models.enums import UserRoleEnum
+
+# 공통 속성
+class CompanyBase(BaseModel):
+    company_name: str
+    company_code: str
+
+
+# 생성 요청 시 사용
+class CompanyCreate(BaseModel):
+    company_name: str
+    company_code: str
+
+
+# 응답 시 사용
+class CompanyRead(CompanyBase):
+    id: int
+    company_name: str
+    company_code: str
+
+    class Config:
+        model_config = ConfigDict(from_attributes=True)
