142332378cb7a7979e188a1783e2ea1da2db372b
jtkim-qesg <jtkim@qesg.co.kr>
2025-06-11 19:55:11 +0900

    Update date deprecated method

--- app/models/base.py
+++ app/models/base.py
@@ -1,20 +1,20 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Boolean, DateTime
-from datetime import datetime
+from datetime import datetime, timezone
 
 Base = declarative_base()
 
 class BaseMixin:
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
 class SoftDeleteMixin:
     is_deleted = Column(Boolean, default=False, nullable=False)
-    deleted_at = Column(DateTime, nullable=True)
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
 
     def soft_delete(self):
         self.is_deleted = True
-        self.deleted_at = datetime.utcnow()
+        self.deleted_at = datetime.now(timezone.utc)
 
     @classmethod
     def get_not_deleted(cls, session):
