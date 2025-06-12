d934e99966ad50c23b3376e110417820be847c9e
jtkim-qesg <jtkim@qesg.co.kr>
2025-06-12 18:42:21 +0900

    Init Site Schema

--- app/schemas/Site.py
+++ app/schemas/Site.py
@@ -1,1 +1,22 @@
-# from pydantic import BaseModel
+# from pydantic import BaseModelfrom enum import Enum
+from pydantic import BaseModel, ConfigDict
+from app.models.enums import UserRoleEnum
+from app.schemas.EstimateInfo import (
+    ReadEstimatesInfo
+)
+
+# 공통 속성
+class SiteBase(BaseModel):
+    company_id: int
+    name: str
+    location: str
+    class Config:
+        model_config = ConfigDict(from_attributes=True)
+
+
+class ReadSite(SiteBase):
+    id: int
+
+class ReadSiteWithEstimate(SiteBase):
+    id: int
+    estimate_info: ReadEstimatesInfo
